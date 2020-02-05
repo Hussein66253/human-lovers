b652e64c230d1ee51ac961fff2318f3a782bc979
hussein <hussein66253@icloud.com>
2020-02-05 17:03:27 +0200

    modifed function in the js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var name = prompt("Lets starat to know each other we are code fellows and you are ?");
 var age = prompt("We welcom to all people on our site and we like to know your age ?");
-var gender = prompt("We want to if you are male or female to help us to contact you ?");
+var gender = prompt("We want to if you are male or female to help us to contact you ?"); 
 var result= confirm("Are you ready to see what make us the best!");
 
 
