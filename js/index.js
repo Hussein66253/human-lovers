e561e62bc18a8b3f3f1ae1f05b775795c5fa91ef
hussein <hussein66253@icloud.com>
2020-02-06 00:49:05 +0200

    modifed WITH IF LOOP

--- js/index.js
+++ js/index.js
@@ -1,17 +1,29 @@
-var name = prompt("Lets starat to know each other we are code fellows and you are ?");
-var age = prompt("We welcom to all people on our site and we like to know your age ?");
-var gender = prompt("We want to if you are male or female to help us to contact you ?"); 
-var result= confirm("Are you ready to see what make us the best!");
+// var name = prompt("Lets starat to know each other we are code fellows and you are ?");
+// var age = prompt("We welcom to all people on our site and we like to know your age ?");
+// var gender = prompt("We want to if you are male or female to help us to contact you ?"); 
+// var result= confirm("Are you ready to see what make us the best!");
 
 
-function onOpen (name, age, gender) {
+// function onOpen (name, age, gender) {
 
-if(result==true){alert("lets get start.")}
-else{
-    alert("okay,maybe onther time")
-window.close();
- }
+// if(result==true){alert("lets get start.")}
+// else{
+//     alert("okay,maybe onther time")
+// window.close();
+//  }
  
-}
+// }
 
-onOpen(name, age, gender); 
+// onOpen(name, age, gender); 
+var language = function () {
+var con = prompt ("Which language do you like to learn in the beginning HTML or CSS?"); 
+    var item;
+    if (con=="HTML"){
+        item ='img src=https://live.staticflickr.com/7174/26937348141_d721215193_b.jpg""'
+    } else if (con=="CSS"){
+        item ='https://p1.pxfuel.com/preview/86/583/812/code-computer-css-it.jpg""'
+    }else {
+         item = '<strong> PLEASE SELECT ONE  </strong>'
+     }
+     return item; 
+}
