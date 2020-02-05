86fe2ad58026620206e6d6a2e10a37a58bb813e4
hussein <hussein66253@icloud.com>
2020-02-05 14:32:29 +0200

    modifiid with js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var name = prompt("Lets starat to know each other we are Constructive & CO and you are ?");
+var age = prompt("We welcom to all people on our site and we like to know your age ?");
+var gender = prompt("We want to if you are male or female to help us to cotact you ?");
+var result= confirm("Are you ready to see what make us the best!");
+
+if(result==true){alert("lets get start.")}
+else{alert("okay,maybe late")
+window.close();
+}
